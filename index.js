9789ac0bec56a128b941692503fdbebdca1705d0
Matthew McKenzie <mojomckenzie@knights.ucf.edu>
2021-01-28 15:20:53 -0500

    added comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const axios = require("axios")
+const axios = require("axios") // require axios library to use in file
 
-axios('https://pokeapi.co/api/v2/pokemon/golduck')
-.then(function (response) {
-  console.log(response.data)
+axios('https://pokeapi.co/api/v2/pokemon/golduck') // send a request to pokemon api for the resource of golduck
+.then(function (response) {   // when promises is resolved take resolved response
+  console.log(response.data)  // log the data aboject of the response
 })
-.catch(function (error) {
-  console.log("Error: " + error);
+.catch(function (error) { // catch any errors thrown from sending the request or receiving the response
+  console.log("Error: " + error); // log the err to console
 });
